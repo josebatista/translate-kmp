c7597c0a42f287ed44cff4da6e857a3a874a7e6e
Jose Pereira <josebatistapereira@gmail.com>
2025-07-14 11:03:46 -0300

    view model and screen

---
+++ iosApp/iosApp/translate/presentation/TranslateScreen.swift
@@ -0,0 +1,60 @@
+//
+//  TranslateScreen.swift
+//  iosApp
+//
+//  Created by Jose Pereira on 7/14/25.
+//
+
+import SwiftUI
+import Shared
+
+struct TranslateScreen: View {
+    private var historyDataSource: HistoryDataSource
+    private var translateUseCase: TranslateUseCase
+    @ObservedObject var viewModel: iOSTranslateViewModel
+    
+    init(historyDataSource: HistoryDataSource, translateUseCase: TranslateUseCase) {
+        self.historyDataSource = historyDataSource
+        self.translateUseCase = translateUseCase
+        self.viewModel = iOSTranslateViewModel(
+            historyDataSource: historyDataSource,
+            translateUseCase: translateUseCase
+        )
+    }
+    
+    var body: some View {
+        ZStack {
+            List {
+                HStack(alignment: .center) {
+                    LanguageDropDown(
+                        language: viewModel.state.fromLanguage,
+                        isOpen: viewModel.state.isChoosingFromLanguage,
+                        selectLanguage: { language in viewModel.onEvent(event: .ChooseFromLanguage(language: language)) }
+                    )
+                    Spacer()
+                    SwapLanguageButton(onClick: { viewModel.onEvent(event: .SwapLanguages()) })
+                    Spacer()
+                    LanguageDropDown(
+                        language: viewModel.state.toLanguage,
+                        isOpen: viewModel.state.isChoosingToLanguage,
+                        selectLanguage: { language in viewModel.onEvent(event: .ChooseToLanguage(language: language)) }
+                    )
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(Color.background)
+            }
+            .listStyle(.plain)
+            .buttonStyle(.plain)
+        }
+        .onAppear {
+            viewModel.startObserving()
+        }
+        .onDisappear {
+            viewModel.dispose()
+        }
+    }
+}
+
+//#Preview {
+//    TranslateScreen()
+//}
